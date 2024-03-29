cca9665c91b931cdccae0702cecb1e201b8c4846
r6t <ryancast@gmail.com>
2024-03-29 15:01:29 -0700

    headscale.py

--- headscale.py
+++ headscale.py
@@ -1,8 +1,17 @@
-from troposphere import Ref, Template
+from troposphere import Parameter, Ref, Template
 from troposphere.ec2 import VPC, Subnet, Instance, InternetGateway
 from troposphere.ec2 import VPCGatewayAttachment, SubnetRouteTableAssociation
-from troposphere.ec2 import RouteTable, Route, SecurityGroup, SecurityGroupIngress
+from troposphere.ec2 import RouteTable, Route, SecurityGroup, SecurityGroupIngress, KeyPair
+import troposphere.ssm
+
 template = Template()
+
+# Define the parameter for KeyPair's public key material
+public_key_parameter = template.add_parameter(Parameter(
+    "PublicKeyMaterial",
+    Description="Public Key Material for EC2 KeyPair",
+    Type="String",
+))
 
 vpc = template.add_resource(VPC(
     "HeadscaleVpc",
@@ -47,29 +56,33 @@ subnet_route_table_association = template.add_resource(SubnetRouteTableAssociati
     RouteTableId=Ref(route_table),
 ))
 
-# Add a security group
 security_group = template.add_resource(SecurityGroup(
     "MySecurityGroup",
     GroupDescription="Allow SSH from specific IPv6 address",
     VpcId=Ref(vpc),
 ))
 
-# Add a security group ingress rule
 security_group_ingress = template.add_resource(SecurityGroupIngress(
     "MySecurityGroupIngress",
     GroupId=Ref(security_group),
     IpProtocol="tcp",
     FromPort="22",
     ToPort="22",
-    CidrIpv6="2601:602:9300:9::1c55/128",  # Replace with your IPv6 address
+    CidrIpv6="2001:558:600a:8b:fca3:bb10:9ff5:6b0a/128",
+))
+
+key_pair = template.add_resource(KeyPair(
+    "HeadscaleEC2Keypair",
+    KeyName="HeadscaleEC2Keypair",
+    PublicKeyMaterial=Ref(public_key_parameter),
 ))
 
 instance = template.add_resource(Instance(
-    "MyInstance",
-    ImageId="ami-0abcdef1234567890",  # replace with your AMI ID
+    "HeadscaleEC2",
+    ImageId="ami-08116b9957a259459",
     InstanceType="t2.micro",
     SubnetId=Ref(subnet),
-    SecurityGroupIds=[Ref(security_group)],  # Add the security group to the instance
+    SecurityGroupIds=[Ref(security_group)],
 ))
 
 with open('cloudformation/headscale-aws.json', 'w') as file:
