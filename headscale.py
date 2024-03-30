5ede22ac81320bead0c61387bd350c8023b7d4d9
r6t <ryancast@gmail.com>
2024-03-29 21:20:26 -0700

    wip vpc

--- headscale.py
+++ headscale.py
@@ -1,24 +1,28 @@
 from troposphere import Parameter, Ref, Template
-from troposphere.ec2 import VPC, Subnet, Instance, InternetGateway
+from troposphere.ec2 import VPC, VPCCidrBlock, Subnet, SubnetCidrBlock, Instance, InternetGateway, KeyPair
 from troposphere.ec2 import VPCGatewayAttachment, SubnetRouteTableAssociation
-from troposphere.ec2 import RouteTable, Route, SecurityGroup, SecurityGroupIngress, KeyPair
-import troposphere.ssm
+from troposphere.ec2 import RouteTable, Route, SecurityGroup, SecurityGroupIngress
 
 template = Template()
 
-# Define the parameter for KeyPair's public key material
 public_key_parameter = template.add_parameter(Parameter(
-    "PublicKeyMaterial",
-    Description="Public Key Material for EC2 KeyPair",
+    "PublicKeyParameter",
+    Description="SSH public key to be used with EC2 keypair",
     Type="String",
 ))
 
 vpc = template.add_resource(VPC(
     "HeadscaleVpc",
-    CidrBlock="2001:db8::/32",  # IPv6 CIDR block
-    EnableDnsSupport="true",
-    EnableDnsHostnames="true",
+    CidrBlock="10.0.0.0/16",
+    EnableDnsSupport=True,
+    EnableDnsHostnames=True,
     InstanceTenancy="default",
+))
+
+vpcCidrBlock = template.add_resource(VPCCidrBlock(
+    "Headscaleipv6CidrBlock",
+    VpcId=Ref(vpc),
+    AmazonProvidedIpv6CidrBlock=True,
 ))
 
 igw = template.add_resource(InternetGateway(
@@ -39,15 +43,22 @@ route_table = template.add_resource(RouteTable(
 route = template.add_resource(Route(
     "Route",
     RouteTableId=Ref(route_table),
-    DestinationCidrBlock="::/0",  # IPv6 for all addresses
+    DestinationCidrBlock="0.0.0.0/0",
     GatewayId=Ref(igw),
 ))
 
 subnet = template.add_resource(Subnet(
-    "MySubnet",
+    "HeadscalePublicSubnet",
     VpcId=Ref(vpc),
-    CidrBlock="2001:db8::/64",  # IPv6 CIDR block
-    MapPublicIpOnLaunch="true",
+    CidrBlock="10.0.1.0/24",
+#    AssignIpv6AddressOnCreation=True,
+    MapPublicIpOnLaunch=True,
+))
+
+EC2SubnetCidrBlock = template.add_resource(SubnetCidrBlock(
+    'EC2SubnetCidrBlock',
+#   Ipv6CidrBlock="2600:1f14:2112:1c00::/56",
+    SubnetId=Ref(subnet),
 ))
 
 subnet_route_table_association = template.add_resource(SubnetRouteTableAssociation(
@@ -58,7 +69,7 @@ subnet_route_table_association = template.add_resource(SubnetRouteTableAssociati
 
 security_group = template.add_resource(SecurityGroup(
     "MySecurityGroup",
-    GroupDescription="Allow SSH from specific IPv6 address",
+    GroupDescription="Allow Headscale instance ingress traffic",
     VpcId=Ref(vpc),
 ))
 
@@ -71,10 +82,11 @@ security_group_ingress = template.add_resource(SecurityGroupIngress(
     CidrIpv6="2001:558:600a:8b:fca3:bb10:9ff5:6b0a/128",
 ))
 
-key_pair = template.add_resource(KeyPair(
-    "HeadscaleEC2Keypair",
-    KeyName="HeadscaleEC2Keypair",
-    PublicKeyMaterial=Ref(public_key_parameter),
+ec2_keypair = template.add_resource(KeyPair(
+    "EC2Keypair",
+    KeyName="HeadscaleSSHPublicKey",
+    PublicKeyMaterial=Ref(public_key_parameter)
+
 ))
 
 instance = template.add_resource(Instance(
@@ -83,7 +95,8 @@ instance = template.add_resource(Instance(
     InstanceType="t2.micro",
     SubnetId=Ref(subnet),
     SecurityGroupIds=[Ref(security_group)],
+    KeyName=Ref(ec2_keypair)
 ))
 
-with open('cloudformation/headscale-aws.json', 'w') as file:
-    file.write(template.to_json())
+with open('cloudformation/headscale.yaml', 'w') as file:
+    file.write(template.to_yaml())
